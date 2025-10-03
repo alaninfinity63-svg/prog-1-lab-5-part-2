c1461674d670bd924d12f9d048402bfdf0edf754
alan <alaninfinity63@gmail.com>
2025-10-03 14:18:41 -0400

    update 1

---
+++ MapTester.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+/**
+ * Write a description of class MapTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MapTester
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MapTester
+     */
+    public MapTester()
+    {
+       HashMap<String, String> contacts = new HashMap<>(); 
+       contacts.put("Cameroon", "Yaounde");
+        contacts.put("Canada", "Toronto");
+        contacts.put("China", "Hong Kong");
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
--- Responder.java
+++ Responder.java
@@ -1,4 +1,8 @@
-
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -8,13 +12,115 @@
  */
 public class Responder
 {
+     // Used to map key words to responses.
+    
+    private HashMap<String, String> responseMap;
+    // Default responses to use if we don't recognise a word.
+    private ArrayList<String> defaultResponses;
+    private Random randomGenerator;
+
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        responseMap = new HashMap<>();
+        defaultResponses = new ArrayList<>();
+        fillResponseMap();
+        fillDefaultResponses();
+        randomGenerator = new Random();
     }
-
+    private void fillResponseMap(){
+        responseMap.put("crash", 
+                        """
+                        Well, it never crashes on our system. It must have something
+                        to do with your system. Tell me more about your configuration.
+                        """);
+        responseMap.put("crashes", 
+                        """
+                        Well, it never crashes on our system. It must have something
+                        "to do with your system. Tell me more about your configuration.
+                        """);
+        responseMap.put("slow", 
+                        """
+                        I think this has to do with your hardware. Upgrading your processor
+                        should solve all performance problems. Have you got a problem with
+                        our software?
+                        """);
+        responseMap.put("performance", 
+                        """
+                        Performance was quite adequate in all our tests. Are you running
+                        any other processes in the background?
+                        """);
+        responseMap.put("bug", 
+                        """
+                        Well, you know, all software has some bugs. But our software engineers
+                        are working very hard to fix them. Can you describe the problem a bit
+                        further?
+                        """);
+        responseMap.put("buggy", 
+                        """
+                        Well, you know, all software has some bugs. But our software engineers
+                        "are working very hard to fix them. Can you describe the problem a bit
+                        further?
+                        """);
+        responseMap.put("windows", 
+                        """
+                        This is a known bug to do with the Windows operating system. Please
+                        report it to Microsoft. There is nothing we can do about this.
+                        """);
+        responseMap.put("mac", 
+                        """
+                        This is a known bug to do with the Mac operating system. Please
+                        report it to Apple. There is nothing we can do about this.
+                        """);
+        responseMap.put("expensive", 
+                        """
+                        The cost of our product is quite competitive. Have you looked around
+                        and really compared our features?
+                        """);
+        responseMap.put("installation", 
+                        """
+                        The installation is really quite straight forward. We have tons of
+                        "wizards that do all the work for you. Have you read the installation
+                        instructions?
+                        """);
+        responseMap.put("memory", 
+                        """
+                        If you read the system requirements carefully, you will see that the
+                        specified memory requirements are 1.5 giga byte. You really should
+                        upgrade your memory. Anything else you want to know?
+                        """);
+        responseMap.put("linux", 
+                        """
+                        We take Linux support very seriously. But there are some problems.
+                        Most have to do with incompatible glibc versions. Can you be a bit
+                        more precise?
+                        """);
+        responseMap.put("bluej", 
+                        """
+                        Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but
+                        they simply won't sell... Stubborn people they are. Nothing we can
+                        do about it, I'm afraid.
+                        """);
+    }
+    private void fillDefaultResponses(){
+        defaultResponses.add("That sounds odd. Could you describe that problem in more detail?");
+        defaultResponses.add("""
+                             No other customer has ever complained about this before.
+                             What is your system configuration?
+                             """);
+        defaultResponses.add("That sounds interesting. Tell me more...");
+        defaultResponses.add("I need a bit more information on that.");
+        defaultResponses.add("Have you checked that you do not have a dll conflict?");
+        defaultResponses.add("That is explained in the manual. Have you read the manual?");
+        defaultResponses.add("""
+                             Your description is a bit wishy-washy. Have you got an expert
+                             there with you who could describe this more precisely?
+                             """);
+        defaultResponses.add("That's not a bug, it's a feature!");
+        defaultResponses.add("Could you elaborate on that?");
+    }
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
